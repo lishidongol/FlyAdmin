fec73bc9de5a1df8f4dad797950a24e96b2da4b2
lishidongol <996479960@qq.com>
2022-07-02 01:07:19 +0800

    初始化提交 FlyAdmin-Server

---
+++ FlyAdmin-Server/src/main/java/com/lishidong/flyadmin/flyadminserver/FlyAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.lishidong.flyadmin.flyadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyAdminServerApplication.class, args);
+    }
+
+}
---
+++ FlyAdmin-Server/src/test/java/com/lishidong/flyadmin/flyadminserver/FlyAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishidong.flyadmin.flyadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
